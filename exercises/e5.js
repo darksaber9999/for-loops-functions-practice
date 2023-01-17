284e4fa82b14c111cf8759f5b705c20681079913
Chris <77562903+darksaber9999@users.noreply.github.com>
2023-01-17 15:06:35 -0700

    Finished exercise 5

--- exercises/e5.js
+++ exercises/e5.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 5
 // Return an array of all bank account holders names
 // Array example: bankAccounts in /data/data.js
@@ -6,10 +5,14 @@
 
 export function getAllClientNames(array) {
   // Your code goes here...
+  const results = [];
 
+  for (const client of array) {
+    results.push(client.name);
+  }
+
+  return results;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
