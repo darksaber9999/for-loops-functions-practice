175982e1792d5db1215970494d75e3d855c09776
Chris <77562903+darksaber9999@users.noreply.github.com>
2023-01-18 03:12:05 -0700

    Finished exercise 16

--- exercises/e16.js
+++ exercises/e16.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 16
 // Please, read the exercise-info/flat.md to get the initial data of what is the expected result of this exercise.
 // Array example: flatArraysData in /data/data.js
@@ -7,10 +6,23 @@
 
 export function flatArrays(array) {
   // Your code goes here...
+  const results = [];
 
+  function flatten(item) {
+    if (!Array.isArray(item)) {
+      results.push(item);
+      return;
+    }
+
+    for (const value of item) {
+      flatten(value);
+    }
+  }
+
+  flatten(array);
+
+  return results;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-16"
