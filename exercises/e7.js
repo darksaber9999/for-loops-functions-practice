73e5517c90d8dcfbc8bb4a07f1c0f29ded210c5e
Chris <77562903+darksaber9999@users.noreply.github.com>
2023-01-17 17:12:01 -0700

    Finished exercise 7

--- exercises/e7.js
+++ exercises/e7.js
@@ -5,7 +5,30 @@
 
 export function getClientWithLeastBalance(array) {
   // Your code goes here...
+  const results = [];
+  let tempMinBalance = 0;
+  let targetIndex = 0;
 
+  // Sets a temporary minimum balance that isn't zero
+  for (const client of array) {
+    if (client.balance === 0) {
+      continue;
+    }
+    tempMinBalance = client.balance;
+    break;
+  }
+
+  // Finds index of minimum non-zero balance
+  for (const client of array) {
+    if (client.balance < tempMinBalance && client.balance !== 0) {
+      tempMinBalance = client.balance;
+      targetIndex = array.indexOf(client);
+    }
+  }
+
+  results.push(array[targetIndex]);
+
+  return results;
 }
 
 // === TEST YOURSELF ===
