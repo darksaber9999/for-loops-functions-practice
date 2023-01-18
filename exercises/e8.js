422752b9ee32a742fbd56f7cd9bbc978613efe58
Chris <77562903+darksaber9999@users.noreply.github.com>
2023-01-17 17:19:58 -0700

    Finished exercise 8

--- exercises/e8.js
+++ exercises/e8.js
@@ -1,4 +1,3 @@
-
 // EXERCISE 8
 // Return an array with a bank account object with the greatest balance value
 // Array example: bankAccounts in /data/data.js
@@ -6,10 +5,21 @@
 
 export function getClientWithGreatestBalance(array) {
   // Your code goes here...
+  const results = [];
+  let tempMaxBalance = 0;
+  let targetIndex = 0;
 
+  for (const client of array) {
+    if (client.balance > tempMaxBalance) {
+      tempMaxBalance = client.balance;
+      targetIndex = array.indexOf(client);
+    }
+  }
+
+  results.push(array[targetIndex]);
+
+  return results;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
